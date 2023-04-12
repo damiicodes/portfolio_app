775f9d68683d3d8c3e93bb6f51944ed6f22cc674
Dami <dami.fajinmi@hotmail.com>
2023-04-12 14:11:13 +0100

    Added a description on the new column

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ with open('about_me.txt') as file:
     content = file.read()
 
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1, 3])
+col3 = st.container()
 
 with col1:
-    st.image('images/Dami_image.png', width=300)
+    st.image('images/Dami_image.png', width=400, use_column_width='auto', output_format='auto')
 
 with col2:
     st.title('Dami Fajinmi')
     st.info(content)
 
 
-
+with col3:
+    text = """ 
+    Below you can find some Python Apps I have built
+    """
+    st.write(text)
