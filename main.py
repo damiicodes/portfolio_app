71ad4d6e4a9f3f04f9d32f9c4a60085d71d1e805
Dami <dami.fajinmi@hotmail.com>
2023-04-14 11:06:29 +0100

    Added background image and css styling

--- main.py
+++ main.py
@@ -6,28 +6,39 @@ st.set_page_config(layout='wide')
 with open('about_me.txt') as file:
     content = file.read()
 
-st.markdown("""
-<style>
-h1 {
-    color: pink;
-    font: monaco
-    font-size: 36px;
-    text-align: center;
-}
-</style>
-""", unsafe_allow_html=True)
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url('https://images.unsplash.com/photo-1628029799784-50d803e64ea0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=659&q=80');
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+
+add_bg_from_url()
 
 
 col1, col2 = st.columns([1, 3])
 col3 = st.container()
 col4, empty_col, col5 = st.columns([1.5, 0.5, 1.5])
 
+
 with col1:
     st.image('images/Dami_image.png', width=400, use_column_width='auto', output_format='auto')
 
 with col2:
     st.title('Dami Fajinmi')
-    st.info(content)
+    st.write(f'<p> {content} </p>', unsafe_allow_html=True)
 
 
 with col3:
