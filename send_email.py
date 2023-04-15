7433a1b3c847e1039b2a01ab6323fd1d0ef4334f
Dami <dami.fajinmi@hotmail.com>
2023-04-15 15:14:45 +0100

    completed contact me form to allow sending of emails

--- send_email.py
+++ send_email.py
@@ -1,23 +1,17 @@
 import smtplib
 import ssl
 
-host = 'smtp.gmail.com'
-port = 465
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+    username = 'damiixox@gmail.com'
+    password = 'ujesmfbbjtcmdosn'
+    receiver = 'damiixox@gmail.com'
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 
-username = 'damiixox@gmail.com'
-password = 'ujesmfbbjtcmdosn'
-
-
-receiver = 'damiixox@gmail.com'
-context = ssl.create_default_context()
-
-message = """ \
-Subject:Hi!
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
