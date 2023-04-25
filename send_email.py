8fa2edbec8af67106a095e80b5fd57a0dac815da
Dami <dami.fajinmi@hotmail.com>
2023-04-25 12:04:14 +0100

    used secret to protect password

--- send_email.py
+++ send_email.py
@@ -1,12 +1,13 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
     host = 'smtp.gmail.com'
     port = 465
     username = 'damiixox@gmail.com'
-    password = 'ujesmfbbjtcmdosn'
+    password = os.getenv('PASSWORD')
     receiver = 'damiixox@gmail.com'
     context = ssl.create_default_context()
 
